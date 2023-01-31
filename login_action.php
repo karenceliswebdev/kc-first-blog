634a5d2113b4f1ae484f7fa9cad85249038a4a60
karenceliswebdev <karen.celis@student.kdg.be>
2023-01-31 23:47:26 +0100

    Login function works

--- login_action.php
+++ login_action.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+
 //eerst zien email bestaat in db
 
 //dan zien ingevulde pw na hashing overeenkomt met hash in db
@@ -9,6 +12,55 @@
 //maak uniek sessie id en steek deze in db 
 
 //steek deze sessie id in een auth cookie (vanaf nu bij iedere site gecheckt hierop (via auth cookie))
+
+include './database.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if(empty($_POST['email'])) {
+
+        header('Location: ./login.php');
+        die;
+    }
+
+    if(empty($_POST['password'])) {
+        
+        header('Location: ./login.php');
+        die;
+    }
+    
+    $user = checkEmailExists($db, $_POST['email']);
+
+    //User does not exist cannot login. Should create an account
+    if(!$user) {
+        
+        //$_SESSION['feedback'] = 'maak een account aan';
+        header('Location: ./login.php');
+        die;
+    }
+
+    //Check if user input in password field hashed is the same as the has from our database. If not password is incorrect
+    if(!password_verify($_POST['password'], $user['hash'])) {
+        
+        //$_SESSION['feedback'] = 'incorrecte logingegevens';
+        header('Location: login.php');
+        die;
+    }
+
+    //Create session id for user
+    $userSessionId = uniqid();
+
+    //sessie id in db stoppen
+    updateSessionId($db, $userSessionId, $_POST['email']);
+
+    //in cookie stoppen
+    setcookie('auth', $userSessionId, time() + 3600, '', '', true);
+
+    //Redirect to page met gebruiker naam in hoek
+    header('Location: ./index_with_account.php');
+
+    
+}
 
 
 //fouten:
