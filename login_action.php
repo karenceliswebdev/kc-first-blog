a3ed39005219857c6d1a3b8cc21a8aba4b8a277e
karenceliswebdev <karen.celis@student.kdg.be>
2023-01-31 22:55:06 +0100

    Added login_action.php

---
+++ login_action.php
@@ -0,0 +1,26 @@
+<?php
+
+//eerst zien email bestaat in db
+
+//dan zien ingevulde pw na hashing overeenkomt met hash in db
+
+//indien 1 en 2 ja verander login met username (hierop klikt krijg je: username + jouw posts + gelikte post links te zien )
+
+//maak uniek sessie id en steek deze in db 
+
+//steek deze sessie id in een auth cookie (vanaf nu bij iedere site gecheckt hierop (via auth cookie))
+
+
+//fouten:
+
+//geen email ingevuld: toon geef email in
+
+//geen paswoord ingevuld toon: geef paswoord in
+
+//indien email niet herkent: toon maak een account aan
+
+//maak een feedback cookie voor elk (email/ww/account)
+
+
+?>
+
