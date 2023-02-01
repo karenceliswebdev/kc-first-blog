cbd29ee2540604005d741137fac4cb12e8d314aa
karenceliswebdev <karen.celis@student.kdg.be>
2023-02-01 20:06:47 +0100

    Added user_posts.php

---
+++ user_posts.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+
+//doel 1 hoop blogposts maken en deze weergeven op homepage
+
+include './database.php';
+include './functions.php';
+
+//all auth cookie gemaakt?
+
+//checken sessie nog geldig anders redirect to login page
+checkSessionStillExists($db);
+
+$posts = getAllPostsFromUser($db, $userId);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+
+    <!--nav-->
+    <ul>
+        <li><a href="./login.php">log in</a></li>
+    </ul>
+
+    <!--recent posts (6)-->
+     <!--recent posts (6)-->
+     <div class="allUserPosts">
+        <?php if(!(count($posts) === 0)) : ?>
+            <?php foreach($posts as $posts) : ?>
+                <h2><?= $posts['title']; ?></h2>
+                <img src="./pictures/pic_default.png" alt="">
+                <p><?= readMore($posts['body']); ?></p>
+
+                <form action="./blog_detail.php" method="post">
+                    <input type="hidden" name="postId" value="<?= $posts['id']; ?>"/>
+                    <button>Read More</button>
+                </form>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+
+</body>
+</html>
