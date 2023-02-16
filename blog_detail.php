903044ed6b8d207e3f3252976cd4cf74a1c6a0b9
karenceliswebdev <karen.celis@student.kdg.be>
2023-02-16 10:56:21 +0100

    Added two if conditions to check if session exists in php section, added two if conditions in nav, added edit and like possibility. So I dont need blog_detail_with_account.php

--- blog_detail.php
+++ blog_detail.php
@@ -6,14 +6,22 @@ include './database.php';
 include './functions.php';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $num = ($_POST['postId']);
-    
-    $post = getPostDetailPage($db, (int)$num);
+    //werken met sessies anders als je terug redirect van action weet ni meer welke postid ah ja geen post dan gebeurt
+    $_SESSION['postId'] = $_POST['postId'];
 }
 
+$post = getPostDetailPage($db, (int)$_SESSION['postId']);
+$sessionExist = checkSessionExists($db);
 
-//doel 1 hoop blogposts maken en deze weergeven op homepage
+if($sessionExist===false) {
+    $user = [''];
+    $userLikedPost = false;
+}
+
+if($sessionExist===true) {
+    $user = getUser($db);
+    $userLikedPost = checkUserLikedPost($db, (int)$_SESSION['postId']);
+}
 
 ?>
 
@@ -27,11 +35,26 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
 
+   <!--nav-->
+   <ul>
+        <?php if($sessionExist===false) : ?>
+            <li><a href="./login.php">log in</a></li>
+        <?php endif; ?>
 
-    <!--nav-->
-    <ul>
-        <li><a href="./login.php">log in</a></li>
+        <?php if($sessionExist===true) : ?>
+            <li><a href="./user_posts.php">your posts</a></li>
+            <li><a href="./liked_posts.php">liked posts</a></li>
+            <li><a href="./login.php">log out</a></li>
+        <?php endif; ?>
     </ul>
+
+    <!--edit possibility-->
+    <?php if($post['user_id']===$user['id']) : ?>
+        <form action="./edit_post.php" method="post">
+            <input type="hidden" name="postId" value="<?= $post['id']; ?>"/>
+            <button>Edit</button>
+        </form>
+    <?php endif; ?>
 
     <!--post-->
     <div class="post">
@@ -39,6 +62,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         <img src="./pictures/pic_default.png" alt="">
         <p><?= $post['body']; ?></p>     
     </div>
+
+    <!--like mss iets doen like tikt zonder user alert login/regi-->
+    <form action="./like_action.php" method="post">
+        <input type="hidden" name="postId" value="<?= $post['id']; ?>"/>
+        <button style="height:50px; width:50px;">
+            <img src = "<?= $userLikedPost ? './pictures/heart-full.svg' : './pictures/heart-empty.svg'; ?>" alt="heart">            
+        </button>
+    </form>
 
 </body>
 </html>
