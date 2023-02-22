ad6f77b107e8a08c526c118873b5b5ad3ab4728b
karenceliswebdev <karen.celis@student.kdg.be>
2023-02-22 22:45:13 +0100

    Added h1

--- pages/edit_post.php
+++ pages/edit_post.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 include '../helpers/database.php';
 
 //checken sessie nog geldig anders redirect to login page
@@ -10,11 +9,9 @@ $sessionExist = checkSessionExists($db);
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if(empty($_POST['postId'])) {
-        
         header('Location: ./blog_detail.php');
         die;
     }
-
     //postID nodig voor te zien welke post we moeten aanpassen
     $_SESSION['postId'] = $_POST['postId'];
 }
@@ -25,18 +22,16 @@ $post = getPostDetailPage($db, (int)$_SESSION['postId']);
 ?>
 <?php include "../templates/nav.php"?>
 
+    <h1>Edit post</h1>
+
     <form method="post" action="../actions/edit_post_action.php">
         <div class="newPost">
             <label for="title">Title:</label><br>
             <input type="text" name ="title" id="title" value="<?= $post['title'];?>"/><br>
-            
             <img src="../pictures/pic_default.png" alt=""><br>
-            
             <label for="body">Content:</label><br>
             <textarea name="body" id="body" rows="50" cols="100"><?= $post['body'];?></textarea><br>
-
             <input type="hidden" name="postId" value="<?= $post['id']; ?>"/>
-            
             <button>save</button> 
         </div>
     </form>
