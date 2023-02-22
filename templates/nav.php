ac90754a7f11eb17c04fce2537293f66b660292d
karenceliswebdev <karen.celis@student.kdg.be>
2023-02-22 19:08:46 +0100

    Added ahref recent posts

--- templates/nav.php
+++ templates/nav.php
@@ -13,12 +13,12 @@
         <?php if($sessionExist===false) : ?>
             <li><a href="./index.php">recent posts</a></li>
             <li><a href="./login.php">log in</a></li>
-        <?php endif; ?>
+        <?php endif;?>
 
         <?php if($sessionExist===true) : ?>
             <li><a href="./index.php">recent posts</a></li>
             <li><a href="./user_posts.php">your posts</a></li>
             <li><a href="./liked_posts.php">liked posts</a></li>
             <li><a href="../actions/logout_action.php">log out</a></li>
-        <?php endif; ?>
+        <?php endif;?>
     </ul>
