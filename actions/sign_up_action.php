81b2a6f8bdfb16acee33443ee5d0267978cc32c3
karenceliswebdev <karen.celis@student.kdg.be>
2023-02-22 22:37:18 +0100

    Added session feedback

--- actions/sign_up_action.php
+++ actions/sign_up_action.php
@@ -7,39 +7,33 @@ $user = [];
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if(empty($_POST['email'])) {
-
+        $_SESSION['feedback'] = 'incorrect registration details';
         header('Location: ../pages/sign_up.php');
         die;
     }
 
     if(empty($_POST['password'])) {
-        
+        $_SESSION['feedback'] = 'incorrect registration details';
         header('Location: ../pages/sign_up.php');
         die;
     }
 
     //zien email nog niet ingenomen
+    $emailExists = checkEmailExists($db, $_POST['email']);
 
-    $user = checkEmailExists($db, $_POST['email']);
-
-    if($user) {
-
+    if($emailExists) {
+        $_SESSION['feedback'] = 'User already exist, go to login';
         //toon: email bestaat al
         header('Location: ../pages/sign_up.php');
         die;
     }
       
     //hash password
- 
     //bestaat niet dus kun je hem in db stoppen
-
     addNewUser($db, $_POST['email'], $_POST['password']);
 
     //Redirect to page met gebruiker naam in hoek
-    
     header('Location: ../pages/login.php');
-
-    
 }
 
 //fouten:
