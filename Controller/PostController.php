d27ca80974e0771bc732d9074df097f9f6ff759f
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-02 17:22:26 +0100

    Placed method with same name as his public function inside this function, after that i slightly altered public function name, deleted returns where there is void

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -6,97 +6,76 @@ session_start();
     
 class PostController extends Post {
 
-    //voor posts te tonen homepage
-    public function getPosts(): array { //post
-        
-        
-    
+    public function get(): array { 
+        return $this->getPosts();
     }
     
-    public function getPostDetailPage(int $postId): array { //post
-        
-        
+    public function getDetails(int $postId): array { //post
+        return $this->getPostDetailPage($postId);
     }
     
     public function getAllPostsFromUser(): array { //user
-        
-        
+        return $this->getPostDetailPage($postId);
     }
     
-    public function updatePost(string $title, string $body, int $postId): void {    //post
-    // string en title html
-       
-    
+    public function update(string $title, string $body, int $postId): void {    //post
+        $this->updatePost($title, $body, $postId);
     }
     
     //zoek eerst user via sessie id (cookie) in db dan voeg je die mee in de query
-    public function addNewPost(string $title, string $body): void { //post
-        
-       
-    
+    public function add(string $title, string $body): void { //post
+        $this->addNewPost($title, $body);
     }
     
     public function deletePost(int $postId): void {
-        
+        $this->deletePost($postId);
     }
     
-    public function checkEmailExists(string $email): bool {
-               
-        
+    public function checkEmail(string $email): bool {
+        return $this->checkEmailExists($email); 
     }
     
     //sessie id in db stoppen
-    public function updateSessionId(string $email): void {
-    
-        
+    public function updateSession(string $email): void {
+        $this->updateSessionId($email); 
     }
     
-    public function checkSessionExists(): bool {
-        
-       
+    public function checkSession(): bool {
+        return $this->checkSessionExists();   
     }
     
     //check hash (input pp) = db hash
-    public function checkUserPasswordCorrect(string $email, string $password): bool {
-        
+    public function checkUserPassword(string $email, string $password): bool {
+        return $this->checkUserPasswordCorrect($email, $password);  
     }
     
     //vind user via session id
-    public function getUser(): array {
-        
-        
+    public function getUserPost(): array {
+        return $this->getUser();  
     }
     
     //voeg user toe die zich heeft geregistreerd
     public function addNewUser(string $email, string $password): void {
-        
-       
-    
+        $this->addNewUser($email, $password);
     }
     
     public function checkUserLikedPost(int $postId): bool {
-        
-       
+        return $this->checkUserLikedPost($postId); 
     }
     
     public function getAllLikedPostsFromUser(): array {
-        
-        
+        return $this->getAllLikedPostsFromUser(); 
     }
     
     public function showLikes(int $postId): void {
-        
-       
+        $this->showLikes($postId); 
     }
     
     public function addLikePost(int $postId): void {
-        
-       
-    
+        $this->addLikePost($postId);
     }
     
     public function deleteLikePost(int $postId): void {   
-   
-        
+        $this->deleteLikePost($postId);
     }
 }
