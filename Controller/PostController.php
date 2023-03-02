e58b1a78b6a84c54aee6d0d6177bcdaa6a4b2aa5
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-02 17:54:49 +0100

    Deleted these functions from PostController: getPosts, checkEmail, updateSession, checkSession, checkUserPassword, getUserPost, addNewUser, checkUserLikedPost, getAllLikedPostsFromUser

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -14,10 +14,6 @@ class PostController extends Post {
         return $this->getPostDetailPage($postId);
     }
     
-    public function getAllPostsFromUser(): array { //user
-        return $this->getPostDetailPage($postId);
-    }
-    
     public function update(string $title, string $body, int $postId): void {    //post
         $this->updatePost($title, $body, $postId);
     }
@@ -29,42 +25,6 @@ class PostController extends Post {
     
     public function deletePost(int $postId): void {
         $this->deletePost($postId);
-    }
-    
-    public function checkEmail(string $email): bool {
-        return $this->checkEmailExists($email); 
-    }
-    
-    //sessie id in db stoppen
-    public function updateSession(string $email): void {
-        $this->updateSessionId($email); 
-    }
-    
-    public function checkSession(): bool {
-        return $this->checkSessionExists();   
-    }
-    
-    //check hash (input pp) = db hash
-    public function checkUserPassword(string $email, string $password): bool {
-        return $this->checkUserPasswordCorrect($email, $password);  
-    }
-    
-    //vind user via session id
-    public function getUserPost(): array {
-        return $this->getUser();  
-    }
-    
-    //voeg user toe die zich heeft geregistreerd
-    public function addNewUser(string $email, string $password): void {
-        $this->addNewUser($email, $password);
-    }
-    
-    public function checkUserLikedPost(int $postId): bool {
-        return $this->checkUserLikedPost($postId); 
-    }
-    
-    public function getAllLikedPostsFromUser(): array {
-        return $this->getAllLikedPostsFromUser(); 
     }
     
     public function showLikes(int $postId): void {
