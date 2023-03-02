1573b508af0acf0754fbe6761dec93b326e8e636
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-03 00:12:11 +0100

    Added if condition to autoload

--- autoload.php
+++ autoload.php
@@ -4,9 +4,12 @@ spl_autoload_register('myAutoloader');
 
 function myAutoloader($className) {
     
+    if(str_contains('Controller', $className)) {
+        $path = '../Controller/';
+    }
+
     $path = '../Models/';
     $extension = '.php';
     $fullPath = $path . $className . $extension;
-    
     include $fullPath;
-};
+}
