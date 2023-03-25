8d0a5d7a8ef68ca4dff3af41effbe7259b064e7a
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-25 22:54:00 +0100

    Deleted getUser on logout-action page

--- actions/logout-action.php
+++ actions/logout-action.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 include '../helpers/database.php';
 
-//moet weten welke user ik sessie wil verwijderen
-$user = getUser($db);
- 
-//Create session id for user
 $userSessionId = uniqid();
 session_destroy();
 
