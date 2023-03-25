d4f1e558260ed1e8f89f3542e8dc2e2959300896
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-25 16:05:13 +0100

    Created head component, placed code lines 1-9 from the nav component to head component

--- pages/components/nav.php
+++ pages/components/nav.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
+<ul>
+    <?php if($sessionExist===false) : ?>
+        <li><a href="./index.php">recent posts</a></li>
+        <li><a href="./login.php">log in</a></li>
+    <?php endif;?>
 
-    <!--nav-->
-    <ul>
-        <?php if($sessionExist===false) : ?>
-            <li><a href="./index.php">recent posts</a></li>
-            <li><a href="./login.php">log in</a></li>
-        <?php endif;?>
-
-        <?php if($sessionExist===true) : ?>
-            <li><a href="./index.php">recent posts</a></li>
-            <li><a href="./user_posts.php">your posts</a></li>
-            <li><a href="./liked_posts.php">liked posts</a></li>
-            <li><a href="../actions/logout_action.php">log out</a></li>
-        <?php endif;?>
-    </ul>
+    <?php if($sessionExist===true) : ?>
+        <li><a href="./index.php">recent posts</a></li>
+        <li><a href="./user_posts.php">your posts</a></li>
+        <li><a href="./liked_posts.php">liked posts</a></li>
+        <li><a href="../actions/logout_action.php">log out</a></li>
+    <?php endif;?>
+</ul>
