a1d6395fa0fb6eae815d99a393b7746ddf5b8485
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-25 17:48:26 +0100

    Created sign-up-form and login-form components, placed form andp href from sign-up page inside sign-up-form, placed form and p href from login page inside login-form

---
+++ pages/components/login-form.php
@@ -0,0 +1,8 @@
+<form method="post" action="../actions/login-action.php">
+    <label for="email">email</label><br>
+    <input type="text" name ="email" id="email"></input><br>
+    <label for="password">password</label><br>
+    <input type="password" name ="password" id="password"></input><br>
+    <button>login</button>
+    <p><a href="./sign-up.php">sign up</a></p>
+</form>
---
+++ pages/components/sign-up-form.php
@@ -0,0 +1,8 @@
+<form method="post" action="../actions/sign-up-action.php">
+    <label for="email">email</label><br>
+    <input type="text" name ="email" id="email"></input><br>
+    <label for="password">password</label><br>
+    <input type="password" name ="password" id="password"></input><br>
+    <button>sign up</button>
+    <p><a href="./login.php">log in</a></p>
+</form>
