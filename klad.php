0c35da02c8c7e7628775bc1a13d4cdc04dc14720
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-26 17:12:40 +0200

    Changed klad.php to Klad.php to test features with classes

--- klad.php
+++ klad.php
@@ -1,99 +1,44 @@
 <?php
 
-session_start();
+include_once('Test.php');
 
-$db = connectDb('root','','first_blog_kc');
+class Klad extends Test {
 
-$sessionExist = checkSessionExists($db);
+  private $greeting = 'hi';
 
-
-function checkSessionExists(PDO $db): bool {
-
-    $res = $db->prepare('SELECT * FROM users WHERE session_id = :sessionId');
-    $res->bindParam(':sessionId', $_COOKIE['auth']);
-    $res->setFetchMode(PDO::FETCH_ASSOC);
-    $res->execute();
-    $user = $res->fetch();
-
-    if($user) {
-        return true;
-        die;
-    }
-
-    return false;
+  public function show() {
+    echo $this->var; //krijg db
+    echo $this->greeting;
+  }
+  
 }
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
+$test = new Klad();
+$test->show();
 
-    if(empty($_POST['title'])) {
 
-        //gaat die nog weten welke post ik edit
-        header('Location: ./klad.php');
-        die;
-    }
+/*
+class KLADTWO {
+    protected $var ='db';
 
-    if(empty($_POST['body'])) {
-        
-        header('Location: ./klad.php');
-        die;
-    }
-    
-    //voeg post toe
+  }
 
-    addNewPost($db, $_POST['title'], $_POST['body']);
+class KLAD extends KLADTWO {
 
-    //Redirect to page met gebruiker naam in hoek
-    
-    header('Location: ./pages/user_posts.php');
+  private $greeting = 'hi';
 
+  public function show() {
+    echo $greeting;
+  }
+  
 }
 
-function connectDb(string $user, string $pass, string $db, string $host = 'localhost'): PDO {
-   
-    try {
-        
-        $connection = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
-        return $connection;
+$test = new KLAD();
+$test->show();
 
-    } 
-    catch (Exception $exception) {
-        
-        echo $exception->getMessage();
-    }
-}
+deze error:
 
-function getUser(PDO $db): array {
+ Undefined variable $greeting
+*/
 
-    $res = $db->prepare('SELECT * FROM users WHERE session_id = :sessionId');
-    $res->bindParam(':sessionId', $_COOKIE['auth']);
-    $res->setFetchMode(PDO::FETCH_ASSOC);
-    $res->execute();
-
-    return $user = $res->fetch();    
-}
-
-?>
-
-<?php include "./templates/nav.php" ?>
-    
-    <h1>Add new post</h1>
-
-    <!--new post-->
-    <form method="post" action="./actions/add_post_action.php">
-        <div class="newPost">
-            <label for="title">Title:</label><br>
-            <input type="text" name ="title" id="title"></input><br>
-            
-            <img src="./pictures/pic_default.png" alt=""><br>
-            
-            <label for="body">Content:</label><br>
-            <textarea name="body" id="body" rows="50" cols="100"></textarea><br>
-
-            <button>save</button> 
-        </div>
-    </form>
-    
-    <a href="./pages/user_posts.php"><button>cancel</button></a>   
-
-<?php include "./templates/footer.php" ?>
-
+//$this->greeting = 'hi'; zo alleen niet in functie is fout
