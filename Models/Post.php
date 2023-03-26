98b1558b10a14ff85f1dc1432583c9c56da06362
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-27 00:16:10 +0200

    Added properties: id, userId, title, body and one method __construct

--- Models/Post.php
+++ Models/Post.php
@@ -4,4 +4,17 @@ include_once('../Models/DB.php');
 
 class Post extends Models\DB {
 
+    private int $id;
+    private string $userId;
+    private string $title;
+    private string $body;
+    
+    public function __construct(int $id = null) {
+
+        if(!empty($id)) {
+
+            $this->find($id);
+        }
+    }
+
 }
