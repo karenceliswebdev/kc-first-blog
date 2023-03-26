5ed000da4e68e291f519f4cdd541731be983077b
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-26 17:14:55 +0200

    Added methods __construct and find

--- Models/User.php
+++ Models/User.php
@@ -10,5 +10,32 @@ class User extends DB {
     private string $email;
     private string $hash;
     private string $sessionId;
-   
+
+    public function __construct(int $id = null) {
+
+        $this->sessionId = $_SESSION['sessionId'];
+
+        if(!empty($id)) {
+
+            $this->find($id);
+        }
+    }
+
+    public function find(int $id): User {  
+    
+        $res = $conn->prepare('SELECT * FROM users WHERE id = :id');
+        $res->bindParam('id', $id);
+        $res->execute();
+
+        $user = $res->fetchObject('Models\User');//steek het erin als object
+
+        if(!empty($user))
+        {
+            $this->id = $user->id;
+            $this->email = $user->email;
+            $this->hash = $user->hash;
+        }
+
+        return $this;
+    }   
 }
