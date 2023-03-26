a66fa8cb1083e8d8e0998edc9201e9ccbe13e4ae
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-26 21:08:08 +0200

    Added use PDO, deleted construct function, assigned values to properties inside connect function

--- Models/DB.php
+++ Models/DB.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace Models;
+use PDO;
 session_start();
 
 class DB {
@@ -11,20 +12,17 @@ class DB {
     private $password;
     private $conn;
 
-    function __construct() {
-       
-        $this->host_name = 'localhost';
-        $this->db_name = 'first_blog_kc';
-        $this->username = 'root';
-        $this->password = '';
-    }
-
     protected function connect(): object
     {
+        $host_name = 'localhost';
+        $db_name = 'first_blog_kc';
+        $username = 'root';
+        $password = 'root';
+
         if(!($this->conn)) {
 
             try {
-                $this->conn = new PDO("mysql:host=$this->host_name;dbname=$this->db_name", $this->username, $this->password);
+                $this->conn = new PDO("mysql:host=$host_name;dbname=$db_name", $username, $password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (Exception $exception) {
                 echo $exception->getMessage();
@@ -33,4 +31,5 @@ class DB {
         return $this->conn;
     }       
 }
+
 
