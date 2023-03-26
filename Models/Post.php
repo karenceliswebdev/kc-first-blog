c5cdf895a9b4c594f363762ce34cf562399b6b35
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-27 00:21:56 +0200

    Added method find

--- Models/Post.php
+++ Models/Post.php
@@ -9,6 +9,33 @@ class Post extends Models\DB {
     private string $title;
     private string $body;
     
+    public function __construct(int $id = null) {
+
+        if(!empty($id)) {
+
+            $this->find($id);
+        }
+    }
+
+    public function find(int $id): Post {  
     
+        $res = $this->connect()->prepare('SELECT * FROM posts WHERE id = :id');
+        $res->bindParam('id', $id);
+        $res->execute();
+
+        $post = $res->fetchObject('Post');
+
+        if(!empty($post))
+        {
+            $this->id = $post->id;
+            $this->userId = $post->user_id;
+            $this->title = $post->title;
+            $this->body = $post->body;
+        }
+
+        return $this;
+    }
+
+
 
 }
