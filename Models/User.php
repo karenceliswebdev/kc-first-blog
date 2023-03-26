82028a85d16051280309136753b43aedc7254e0f
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-26 17:14:09 +0200

    Added a namespace, include and properties: id, email, hash, sessionId

--- Models/User.php
+++ Models/User.php
@@ -1,5 +1,14 @@
 <?php
 
+namespace Models;
+
+include_once('./DB.php');
+
 class User extends DB {
 
+    private int $id;
+    private string $email;
+    private string $hash;
+    private string $sessionId;
+   
 }
