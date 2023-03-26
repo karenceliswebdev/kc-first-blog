aad6dd3bd00288bbab74642eaa96cfc9a7b7e711
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-27 00:20:16 +0200

    fixed wrong datatype

--- Models/Post.php
+++ Models/Post.php
@@ -5,16 +5,10 @@ include_once('../Models/DB.php');
 class Post extends Models\DB {
 
     private int $id;
-    private string $userId;
+    private int $userId; 
     private string $title;
     private string $body;
     
-    public function __construct(int $id = null) {
-
-        if(!empty($id)) {
-
-            $this->find($id);
-        }
-    }
+    
 
 }
