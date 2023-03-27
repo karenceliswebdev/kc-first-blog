5a07934b041635c2285aa6b014f93be698b29a07
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-27 20:34:11 +0200

    Replaced condition parameter with session variable

--- pages/components/nav.php
+++ pages/components/nav.php
@@ -1,10 +1,10 @@
 <ul>
-    <?php if($user->findSession()===false) : ?>
+    <?php if(empty($_SESSION['sessionId'])) : ?>
         <li><a href="./index.php">recent posts</a></li>
         <li><a href="./login.php">log in</a></li>
     <?php endif;?>
 
-    <?php if($user->findSession()===true) : ?>
+    <?php if(!empty($_SESSION['sessionId'])) : ?>
         <li><a href="./index.php">recent posts</a></li>
         <li><a href="./user-posts.php">your posts</a></li>
         <li><a href="./liked-posts.php">liked posts</a></li>
