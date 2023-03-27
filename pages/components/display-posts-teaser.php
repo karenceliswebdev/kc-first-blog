8b3db854c50551cb7980a053986715309dc2fdd2
karenceliswebdev <karen.celis@student.kdg.be>
2023-03-27 14:59:13 +0200

    changed posts to nr => post

--- pages/components/display-posts-teaser.php
+++ pages/components/display-posts-teaser.php
@@ -1,6 +1,6 @@
 <div class="recentPosts">
     <?php if(!(count($posts) === 0)) : ?>
-        <?php foreach($posts as $posts) : ?>
+        <?php foreach($posts as $nr => $post) : ?>
             <?php include "./components/post-teaser.php"?>
         <?php endforeach; ?>
     <?php endif; ?>
